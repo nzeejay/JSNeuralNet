8e33c3c24dae9bba1ee8d19f6a5a1441ad5b612e
nzeejay <nzeejay@hotmail.com>
2018-04-15 02:39:02 +1000

    Add files via upload

---
+++ NeuralNetwork.js
@@ -0,0 +1,117 @@
+class neuralNetwork {
+  constructor(layerInfo) {
+    this.layers = new Array(layerInfo.length);
+
+    for (let i = 0; i < this.layers.length; i++) {
+      this.layers[i] = new layer(layerInfo[i], (layerInfo[i - 1] ? layerInfo[i - 1] : null));
+    }
+  }
+
+  clear() {
+    for (let i = 0; i < this.layers.length; i++) {
+      this.layers[i].clear();
+    }
+  }
+
+  forward() {
+    for (let i = 1; i < this.layers.length; i++)
+      this.layers[i].calc(this.layers[i - 1]);
+  }
+
+  backprop(step) {
+    let lastLayer = this.layers.length - 1;
+
+    for (let i = lastLayer; i > 0; i--)
+      this.layers[i].back(this.layers[i - 1], step);
+  }
+
+  setError(expected) {
+    let lastLayer = this.layers.length - 1;
+
+    let totalErr = 0;
+
+    for (let i = 0; i < expected.length; i++) {
+      let val = this.layers[lastLayer].nodes[i] - expected[i];
+      this.layers[lastLayer].error[i] = val;
+      totalErr += val * val;
+    }
+
+    return totalErr / expected.length;
+  }
+
+  query(input) {
+    this.clear();
+
+    this.layers[0].nodes = input;
+
+    this.forward();
+
+    return this.layers[this.layers.length - 1].nodes.slice();
+  }
+}
+
+class layer {
+  constructor(layerInfo, prevLayer) {
+    this.nodes = new Array(layerInfo.size);
+    this.bias = new Array(layerInfo.size);
+
+    this.error = new Array(layerInfo.size);
+
+    for (let i = 0; i < this.bias.length; i++)
+      this.bias[i] = Math.random() * 2 - 1;
+
+    if (prevLayer) {
+      this.weights = new Array(layerInfo.size * prevLayer.size);
+
+      for (let i = 0; i < this.weights.length; i++)
+        this.weights[i] = Math.random() * 2 - 1;
+    }
+
+    this.clear();
+  }
+
+  clear() {
+    for (let i = 0; i < this.nodes.length; i++) {
+      this.nodes[i] = 0;
+      this.error[i] = 0;
+    }
+  }
+
+  calc(prevLayer) {
+    for (let i = 0; i < this.nodes.length; i++) {
+      for (let j = 0; j < prevLayer.nodes.length; j++) {
+        let weightIndex = (i * prevLayer.nodes.length) + j;
+        this.nodes[i] += prevLayer.nodes[j] * this.weights[weightIndex];
+      }
+
+      this.nodes[i] = this.sigmoid(this.nodes[i] + this.bias[i]);
+    }
+  }
+
+  back(prevLayer, step) {
+    for (let i = 0; i < this.nodes.length; i++) {
+      for (let j = 0; j < prevLayer.nodes.length; j++) {
+        let weightIndex = (i * prevLayer.nodes.length) + j;
+
+        let gradient = this.sigmoidDer(prevLayer.nodes[j]) * this.error[i];
+
+        this.bias[i] += -step * gradient;
+
+        this.weights[weightIndex] += -step * prevLayer.nodes[j] * gradient;
+
+        prevLayer.error[j] += this.weights[weightIndex] * gradient;
+
+        constrain(this.bias[i], -5, 5);
+        constrain(this.weights[weightIndex], -5, 5);
+      }
+    }
+  }
+
+  sigmoid(f) {
+    return 1 / (1 + Math.exp(-f));
+  }
+
+  sigmoidDer(f) {
+    return f * (1 - f);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,54 @@
+let nn;
+
+const trainingSamples = [{ input: [0, 0], output: [0] },
+{ input: [1, 0], output: [1] },
+{ input: [0, 1], output: [1] },
+{ input: [1, 1], output: [0] }];  
+
+let size;
+
+let lSlider;
+
+function setup() {
+
+  nn = new neuralNetwork([{ size: 2 }, { size: 32 }, { size: 1 }]);
+
+  size = 600 / 20;
+
+  createCanvas(600, 600);
+
+  noStroke();
+
+  lSlider = createSlider(1, 5000, 1000);
+}
+
+
+
+function draw() {
+
+  for (let i = 0; i < 100; i++) {
+  train();
+  }
+
+  for (let x = 0; x < width; x += size) {
+    for (let y = 0; y < height; y += size) {
+      nn.clear();
+      fill(nn.query([x / width, y / height])[0] * 255);
+      rect(x, y, size, size);
+    }
+  }
+}
+
+function train() {
+  let totalErr = 0;
+
+  for (let i = 0; i < trainingSamples.length; i++) {
+    nn.clear();
+    nn.query(trainingSamples[i].input.slice());
+
+    totalErr += nn.setError(trainingSamples[i].output);
+    nn.backprop(lSlider.value() / 1000);
+  }
+
+  //console.log("err: " + Math.sqrt(totalErr / trainingSamples.length));
+}
