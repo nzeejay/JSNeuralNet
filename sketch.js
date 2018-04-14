3b10f622b45019b166e52938dba9d096d4ad93ac
nzeejay <nzeejay@hotmail.com>
2018-04-15 03:04:02 +1000

    Add files via upload
    Updated controls

--- sketch.js
+++ sketch.js
@@ -8,6 +8,10 @@ const trainingSamples = [{ input: [0, 0], output: [0] },
 let size;
 
 let lSlider;
+let learnRate = 0.100;
+
+let hSlider;
+let currentHidden = 3;
 
 function setup() {
 
@@ -19,18 +23,41 @@ function setup() {
 
   noStroke();
 
+  createP("learn rate: 0.1000");
   lSlider = createSlider(1, 5000, 1000);
+  lSlider.position(125, 620);
+
+  createP("hidden layers: " + currentHidden);
+  hSlider = createSlider(3, 64, 3, 1);
+  hSlider.position(125, 655);
+  
 
   let button = createButton('reload');
   button.mousePressed(setupNet);
 }
 
 function setupNet() {
-  nn = new neuralNetwork([{ size: 2 }, { size: 16 }, { size: 1 }]);  
+  nn = new neuralNetwork([{ size: 2 }, { size: currentHidden }, { size: 1 }]);  
 }
 
+function learnRateListener() {
+  if(lSlider.value() / 10000 != learnRate) {
+    learnRate = lSlider.value() / 10000;
+    document.getElementsByTagName('p')[0].innerHTML = "learn rate: " + learnRate;
+  }
+}
+
+function hiddenListener() {
+  currentHidden = hSlider.value();
+  if(currentHidden != nn.layers[1].nodes.length) {
+    setupNet();
+    document.getElementsByTagName('p')[1].innerHTML = "hidden layers: " + currentHidden;
+  }
+}
 
 function draw() {
+  learnRateListener();
+  hiddenListener();
 
   for (let i = 0; i < 100; i++) {
   train();
@@ -53,7 +80,7 @@ function train() {
     nn.query(trainingSamples[i].input.slice());
 
     totalErr += nn.setError(trainingSamples[i].output);
-    nn.backprop(lSlider.value() / 1000);
+    nn.backprop(learnRate);
   }
 
   //console.log("err: " + Math.sqrt(totalErr / trainingSamples.length));
